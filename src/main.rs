aeb8cbecfa22ac6d10551ce9f065657e042b8ec6
Colin Knox <colin.r.knox@gmail.com>
2023-07-21 09:03:46 -0500

    command line args handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<_> = env::args().collect();
+    if args.len() > 2 {
+        println!("Usage: lox [script]");
+    } else if args.len() == 2 {
+        run_file(&args[1]);
+    } else {
+        run_prompt();
+    }
 }
+
+fn run_file(file: &String) {}
+
+fn run_prompt() {}
