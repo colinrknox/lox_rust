c3569496b487f643dd16ac4606bbf928f20ed314
Colin Knox <colin.r.knox@gmail.com>
2023-07-21 16:18:13 -0500

    added tokens

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,79 @@
-use std::{env, fs};
+use std::{env, fmt, fs};
+
+#[derive(Debug)]
+enum TokenType {
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    Comma,
+    Dot,
+    Minus,
+    Plus,
+    Semicolon,
+    Slash,
+    Star,
+
+    Bang,
+    BangEqual,
+    Equal,
+    EqualEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
+
+    Identifier,
+    String,
+    Number,
+
+    And,
+    Class,
+    Else,
+    False,
+    Fn,
+    For,
+    If,
+    Nil,
+    Or,
+    Print,
+    Return,
+    Super,
+    This,
+    True,
+    Var,
+    While,
+
+    EOF,
+}
+
+struct Token {
+    r#type: TokenType,
+    lexeme: String,
+    line: usize,
+}
+
+impl Token {
+    fn new(r#type: TokenType, lexeme: String, line: usize) -> Token {
+        Token {
+            r#type,
+            lexeme,
+            line,
+        }
+    }
+}
+
+impl fmt::Display for TokenType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl fmt::Display for Token {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} {} {}", self.r#type, self.lexeme, self.line)
+    }
+}
 
 fn main() {
     let args: Vec<_> = env::args().collect();
@@ -22,9 +97,19 @@ fn run_prompt() {
         if let Ok(line) = std::io::read_to_string(std::io::stdin()) {
             run(line);
         } else {
-            print!("Error: invalid expression");
+            println!("Error: invalid expression");
         };
     }
 }
 
-fn run(code: String) {}
+fn run(code: String) {
+    let mut tokens: Vec<Token> = scan_tokens(code);
+
+    for t in tokens {
+        println!("{}", t);
+    }
+}
+
+fn scan_tokens(code: String) -> Vec<Token> {
+    Vec::new()
+}
