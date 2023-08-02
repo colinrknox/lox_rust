1799a62fe0356ae050fb71143f437189518af27b
Colin Knox <colin.r.knox@gmail.com>
2023-08-02 14:38:17 -0500

    moved api into lib and out of main, added wasm

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,48 @@
+use crate::{lox::Lox, scanner::Scanner, token::Token};
+
+use std::{
+    fs,
+    io::{self, Write},
+    process,
+};
+use wasm_bindgen::prelude::*;
+
 pub mod lox;
 pub mod scanner;
 pub mod token;
+
+pub fn run_file(file: &String) {
+    let contents: String = fs::read_to_string(file).unwrap();
+    let _ = run(contents);
+}
+
+pub fn run_prompt() {
+    loop {
+        print!("> ");
+        let _ = io::stdout().flush();
+        let mut buffer = String::new();
+        let _ = io::stdin().read_line(&mut buffer);
+        if buffer == "exit\n" {
+            break;
+        }
+        let _ = run(buffer);
+    }
+}
+
+fn run(code: String) -> Result<(), String> {
+    let mut scanner = Scanner::new(code, Lox::new());
+    let error = scanner.get_errors();
+    let tokens: &Vec<Token> = scanner.scan_tokens();
+    if error.had_error {
+        process::exit(1);
+    }
+    for t in tokens {
+        println!("{}", t);
+    }
+    Ok(())
+}
+
+#[wasm_bindgen]
+pub fn web_run() {
+    run_prompt();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
-use lox_rust::{lox::Lox, scanner::Scanner, token::Token};
-use std::{
-    env, fs,
-    io::{self, Write},
-    process,
-};
+use lox_rust::{run_file, run_prompt};
+use std::env;
 
 fn main() {
     let args: Vec<_> = env::args().collect();
@@ -14,35 +10,4 @@ fn main() {
     } else {
         run_prompt();
     }
-}
-
-fn run_file(file: &String) {
-    let contents: String = fs::read_to_string(file).unwrap();
-    let _ = run(contents);
-}
-
-fn run_prompt() {
-    loop {
-        print!("> ");
-        let _ = io::stdout().flush();
-        let mut buffer = String::new();
-        let _ = io::stdin().read_line(&mut buffer);
-        if buffer == "exit\n" {
-            break;
-        }
-        let _ = run(buffer);
-    }
-}
-
-fn run(code: String) -> Result<(), String> {
-    let mut scanner = Scanner::new(code, Lox::new());
-    let error = scanner.get_errors();
-    let tokens: &Vec<Token> = scanner.scan_tokens();
-    if error.had_error {
-        process::exit(1);
-    }
-    for t in tokens {
-        println!("{}", t);
-    }
-    Ok(())
 }
