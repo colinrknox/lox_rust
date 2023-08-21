e4f67c865af591b0156e5d024e099e87b00979a2
Colin Knox <colin.r.knox@gmail.com>
2023-08-20 20:19:48 -0500

    expression evaluator started

--- src/ast.rs
+++ src/ast.rs
@@ -1,7 +1,8 @@
 use core::fmt::Display;
 
-use super::token::Token;
+use super::token::{Token, TokenType};
 
+#[derive(Clone)]
 pub enum Object {
     Number(f64),
     String(String),
@@ -9,6 +10,7 @@ pub enum Object {
     Nil,
 }
 
+#[derive(Clone)]
 pub enum Expr {
     Binary(Box<Expr>, Token, Box<Expr>),
     Grouping(Box<Expr>),
@@ -41,5 +43,63 @@ impl Display for Expr {
             Expr::Literal(obj) => write!(f, "{}", obj),
             Expr::Unary(op, rhs) => write!(f, "({} {})", op.get_lexeme(), rhs),
         }
+    }
+}
+
+pub fn eval(expr: Expr) -> Result<Object, Expr> {
+    match expr {
+        Expr::Unary(sign, e) => eval_unary(sign.clone(), *e),
+        Expr::Literal(obj) => Ok(obj),
+        Expr::Binary(lhs, op, rhs) => eval_binary(*lhs, op.clone(), *rhs),
+        Expr::Grouping(e) => eval_grouping(*e),
+    }
+}
+
+fn eval_grouping(expr: Expr) -> Result<Object, Expr> {
+    eval(expr)
+}
+
+fn eval_binary(lhs: Expr, op: Token, rhs: Expr) -> Result<Object, Expr> {
+    let lhs_res = eval(lhs.clone())?;
+    let rhs_res = eval(rhs.clone())?;
+
+    if let (Object::Number(lhs_res), Object::Number(rhs_res)) = (lhs_res.clone(), rhs_res.clone()) {
+        return match op.token_type {
+            TokenType::Minus => Ok(Object::Number(lhs_res - rhs_res)),
+            TokenType::Slash => Ok(Object::Number(lhs_res / rhs_res)),
+            TokenType::Star => Ok(Object::Number(lhs_res * rhs_res)),
+            TokenType::Plus => Ok(Object::Number(lhs_res + rhs_res)),
+            _ => Err(Expr::Binary(Box::new(lhs), op, Box::new(rhs))),
+        };
+    } else if let (Object::String(lhs_res), Object::String(rhs_res)) = (lhs_res, rhs_res) {
+        return match op.token_type {
+            TokenType::Plus => Ok(Object::String(format!("{}{}", lhs_res, rhs_res))),
+            _ => Err(Expr::Binary(Box::new(lhs), op, Box::new(rhs))),
+        };
+    } else {
+        return Err(Expr::Binary(Box::new(lhs), op, Box::new(rhs)));
+    }
+}
+
+fn eval_unary(token: Token, expr: Expr) -> Result<Object, Expr> {
+    let res = eval(expr.clone())?;
+    match token.token_type {
+        TokenType::Minus if matches!(res, Object::Number(_)) => {
+            if let Object::Number(n) = res {
+                Ok(Object::Number(-1.0 * n))
+            } else {
+                Err(expr)
+            }
+        }
+        TokenType::Bang => Ok(is_truthy(res)),
+        _ => Err(expr),
+    }
+}
+
+fn is_truthy(obj: Object) -> Object {
+    match obj {
+        Object::Nil => Object::Boolean(false),
+        Object::Boolean(b) => Object::Boolean(b),
+        _ => Object::Boolean(true),
     }
 }
